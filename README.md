032a83022a2795f92a82e6d94b2c251031cb8292
Bios Sun <biossun@gmail.com>
2016-11-06 00:30:50 +0800

    添加 READM.md 文件，并将注释中的某些内容挪到 README.md 文件中。

---
+++ README.md
@@ -0,0 +1,92 @@
+# Animate.js
+
+Animate.js 是一个非常小的 JS 动画库，其核心部分压缩后仅 **1.7kb**，而加上其动画扩展相关的代码，也只有不到 `5kb` 的大小。
+
+## 理念
+
+Animate.js 期望抽象出一组**动画控制逻辑**，并同时保持自身足够得轻量小巧，通过保证这两点，使其可以集成在**任何项目**中并为其提供动画支持，如应用在 DOM 元素动画、Canvas 动画等**任何目标**的动画效果的实现上。
+
+该库所提供的控制逻辑有如下 5 个：
+
+- 播放：用于开始播放动画；
+- 暂停：用于在动画播放过程中暂停播放；
+- 继续：用于在动画播放暂停之后继续播放；
+- 结束：用于直接结束动画播放；
+- 跳转：用于跳转到动画播放的任一时间节点；
+- 循环：用于控制循环播放动画。
+
+## 示例
+
+```
+var box = document.getElementById('box');
+ 
+var animate = new Animate({
+    speed: 1000,   // or 'slow', 'normal', 'fast'
+    easing: 'linear',  // !optional
+    iteration: 1,      // !optional
+ 
+    // 动画开始时的回调函数
+    begin: function() {
+        box.style.left = '0px';
+    }
+ 
+    // 动画进行中每一帧的回调函数
+    frame: function(progress, easing) {
+        box.style.left = (100 * easing) + 'px';
+    },
+ 
+    // 动画完成时的回调函数
+    finish: function() {
+        box.style.left = '100px';
+    },
+ 
+    // 动画暂停时的回调函数
+    pause: function() {}
+});
+ 
+animate.run();    // 开始动画
+animate.pause();    // 暂停动画
+```
+
+## 接口
+
+### new Animate(options)
+
+创建一个动画对象。
+
+#### 参数
+
+##### `options` {object}
+
+配置对象，支持配置项如下：
+
+- `speed {number | 'slow' | 'normal' | 'fast'}` - 动画时长，以毫秒为单位，另外也可以使用预定的速度关键字。
+- `easing {string}` - 动画缓动效果，核心库只提供 `linear` 及 `swing` 两种效果，默认为 `swing`。若需更多效果，可以额外加载动画扩展库。
+- `iteration {number | 'infinite'}` - 动画播放次数，可将其指定为 `'infinite'` 以开启无限循环模式。默认为 `1`。
+- `begin {function()}` - 播放开始时的回调函数；将在动画开始前（既第一帧前）执行。
+- `finish {function()}` - 播放结束时的回调函数，将在动画结束后（既最后一帧后）执行。
+- `pause {function()}` - 播放暂停时的回调函数，将在暂停功能触发后执行。
+- `frame {function(progress, easing)}` - 帧计算回调函数；将在动画播放到每一帧时回调该函数，通过该函数，您可以渲染相关的动画目标。其中有两个参数：`progress` 表示当前播放进度，其值在 [0, 1] 区间内，`easing` 表示当前的缓动进度，在有的缓动效果中，其值有可能超出 [0, 1] 区间的范围。
+
+### animate.run()
+
+播放动画，若当前播放被暂停，则会在暂停的位置继续播放。
+
+### animate.pause()
+
+暂停动画。
+
+### stop(isToEnd)
+
+结束动画。
+
+#### 参数
+
+##### `isToEnding` {boolean}
+
+是否立即完成动画。默认为 false。
+
+p.s.
+
+无论是否立即完成，都会触发 `finish` 回调函数。
+
--- animate.js
+++ animate.js
@@ -1,49 +1,5 @@
 /***
- * Animate.js 用于管理单个动画播放，支持暂停，继续，延迟播放，立即结束等相关操作。
- * 同时会提供大量事件用于监听动画播放中的每一个状态改变。
- *
- * ## 动画状态
- * - 未开始：动画刚创建时处于未开始状态，可使用 run() 方法执行该动画
- * - 执行中：当调用 run() 方法后，动画处于正在执行中状态
- * - 暂停：在动画执行中时，如果调用 pause() 方法，动画将会暂停播放，此时处于暂停状态，暂停后可调用 resume() 方法继续播放动画。
- * - 结束：在动画播放完成后，将处于结束状态，一个动画只能播放以此，因此结束后的动画不能再次播放。
- *
- * ## 关于动画队列
- * Animate 的目的是用于管理单个动画操作，而队列管理是另外一件事情，因此不会支持（当然也不支持 Promise）。
- * 在文档中有提供一个使用 Promise 编写的队列管理模块，可以以此为参考自行实现。
- *
- * @author: Bios Sun <biossun@gmail.com>
- *
- * @example
- *     var box = document.getElementById('box');
- *
- *     var animate = new Animate({
- *         speed: 1000,   // or 'slow', 'normal', 'fast'
- *         easing: 'linear',  // !optional
- *         iteration: 1,      // !optional
- *
- *         // 动画开始时的回调函数
- *         begin: function() {
- *             box.style.left = '0px';
- *         }
- *
- *         // 动画进行中每一帧的回调函数
- *         frame: function(progress, easing) {
- *             box.style.left = (100 * easing) + 'px';
- *         },
- *
- *         // 动画完成时的回调函数
- *         finish: function() {
- *             box.style.left = '100px';
- *         },
- *
- *         // 动画暂停时的回调函数
- *         pause: function() {}
- *     });
- *
- *     animate.run();    // 开始动画
- *     animate.pause();    // 暂停动画
- *
+ * Animate.js - v1.0.1 - Bios Sun <biossun@gmail.com>
  */
 (function(scope, factory) {
 
@@ -80,31 +36,7 @@
     /**
      * 动画处理函数
      *
-     * @params:
-     *   options : {Object} :
-     *     配置对象
-     *
-     * @options:
-     *
-     *   speed : {Number, ["slow", "normal", "fast"]} :
-     *     动画时长；数值类型，以毫秒为单位；另外也可以使用预定的速度关键字字符串。
-     *
-     *   easing : {String} : 'linear'
-     *
-     *   iteration : {Number, ["infinite"]} : 1
-     *     动画执行次数，另外也可以使用预定关键字字符串“infinite”来设定动画无限循环。
-     *
-     *   begin : {Function} :
-     *     初始化函数；将在动画开始前（第一帧计时开始时）执行。
-     *
-     *   frame : {Function} :
-     *     帧计算函数；将在动画每一帧计时结束后执行。
-     *
-     *   finish : {Function} :
-     *     清理函数；将在动画结束后（最后一帧完成时）执行；
-     *
-     *   pause : {Function} :
-     *     暂停处理函数；将在动画暂停时执行；
+     * @param {object} options - 配置对象
      */
     function Animate( options ) {
         var t = this;
@@ -201,21 +133,17 @@
         /**
          * 结束动画
          *
-         * @params:
-         *   toEnd : {Boolean} : false
-         *     是否将动画立即完成。
-         *
-         *     p.s.
-         *     无论动画是否立即完成，结束回调函数都会被调用。
+         * @param {boolean} - isToEnding - 是否将动画立即完成，默认为 false。
+         *                                 p.s. 无论动画是否立即完成，结束回调函数都会被调用。
          */
-        stop : function( toEnd ) {
+        stop : function( isToEnding ) {
             var t = this;
 
             if (t.isRun === false && t.isPause === false) {
                 return;
             }
 
-            if ( toEnd ) {
+            if ( isToEnding ) {
                t._frame( 1 );
             }
 
